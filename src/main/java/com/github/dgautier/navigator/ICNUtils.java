4880694c75bed0733cd926a9abe317b992a00efa
dgautier <david.gautier@ginko.io>
2016-02-25 11:40:21 +0100

    Removed old comments and platform related informations

---
+++ src/main/java/com/github/dgautier/navigator/ICNUtils.java
@@ -0,0 +1,111 @@
+package com.github.dgautier.navigator;
+
+import com.github.dgautier.icn.exception.InvalidPluginException;
+import com.github.dgautier.icn.plugin.AbstractPlugin;
+import com.github.dgautier.navigator.model.JsonResponse;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import org.apache.http.ParseException;
+import org.apache.http.client.CookieStore;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.protocol.ClientContext;
+import org.apache.http.cookie.Cookie;
+import org.apache.http.protocol.HttpContext;
+
+import java.io.File;
+import java.io.IOException;
+import java.lang.reflect.Modifier;
+import java.net.JarURLConnection;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.util.List;
+import java.util.jar.Attributes;
+import java.util.jar.JarFile;
+import java.util.jar.Manifest;
+
+/**
+ * Created by DGA on 22/01/2015.
+ */
+public class ICNUtils {
+
+    public static JsonResponse fromJSON(String responseJSON) throws ParseException, IOException {
+        Gson gson = new Gson();
+        return gson.fromJson(responseJSON, JsonResponse.class);
+    }
+
+
+    public static String cleanResponse(String responseEntity) {
+        String responseJSON = responseEntity.split("&&")[1];
+        return responseJSON;
+    }
+
+
+    public static void addFormHeader(HttpPost post) {
+        post.addHeader("Cache-Control", "no-cache");
+        String contentType = "application/x-www-form-urlencoded; charset=UTF-8";
+        post.addHeader("Content-Type", contentType);
+    }
+
+
+    public static AbstractPlugin loadFromJar(File pluginFile) throws InvalidPluginException {
+        URL pluginURL;
+        try {
+            pluginURL = pluginFile.toURI().toURL();
+
+            URL jarURL = new URL("jar:" + pluginURL.toString() + "!/");
+            JarURLConnection jarLib = (JarURLConnection) jarURL.openConnection();
+            JarFile jarLibFile = jarLib.getJarFile();
+            Manifest jarManifest = jarLibFile.getManifest();
+            Attributes jarAttributes = jarManifest.getMainAttributes();
+            String pluginClassName = jarAttributes.getValue("Plugin-Class");
+            jarLibFile.close();
+            if (pluginClassName == null) {
+                throw new InvalidPluginException("Invalid IBM Content Navigator plug-in -- missing Plugin-Class attribute in manifest.mf");
+            }
+
+            URLClassLoader pluginClassLoader = URLClassLoader.newInstance(new URL[]{pluginURL}, ICNUtils.class.getClassLoader());
+            Class pluginClass = pluginClassLoader.loadClass(pluginClassName);
+
+            AbstractPlugin plugin = (AbstractPlugin) pluginClass.newInstance();
+            if (plugin != null) {
+                plugin.setJarFile(jarLibFile);
+            }
+            return plugin;
+
+        } catch (IllegalAccessException e) {
+            throw new InvalidPluginException("Unable to load Plugin configuration from jar."
+
+                    + System.lineSeparator() + pluginFile, e);
+        } catch (InstantiationException e) {
+            throw new InvalidPluginException("Unable to load Plugin configuration from jar."
+                    + System.lineSeparator() + pluginFile, e);
+        } catch (ClassNotFoundException e) {
+            throw new InvalidPluginException("Unable to load Plugin configuration from jar."
+                    + System.lineSeparator() + pluginFile, e);
+        } catch (IOException e) {
+            throw new InvalidPluginException("Unable to load Plugin configuration from jar."
+                    + System.lineSeparator() + pluginFile, e);
+        }
+    }
+
+    public static String getPluginJSON(AbstractPlugin plugin) {
+        Gson gson = new GsonBuilder().excludeFieldsWithModifiers(Modifier.PRIVATE).create();
+        return gson.toJson(plugin);
+
+    }
+
+
+    public static void printCookies(HttpContext localContext) {
+        CookieStore cookieStore = (CookieStore) localContext.getAttribute(ClientContext.COOKIE_STORE);
+        if (cookieStore != null) {
+            List<Cookie> cookies = cookieStore.getCookies();
+            if (cookies.isEmpty()) {
+                System.out.println("None");
+            } else {
+                for (int i = 0; i < cookies.size(); i++) {
+                    System.out.println("- " + cookies.get(i).toString());
+                }
+            }
+        }
+    }
+}
---
+++ src/main/java/com/github/dgautier/navigator/maven/plugin/DeployPlugin.java
@@ -0,0 +1,251 @@
+package com.github.dgautier.navigator.maven.plugin;
+
+import com.github.dgautier.icn.plugin.AbstractPlugin;
+import com.github.dgautier.maven.plugin.CopyTargetMojo;
+import com.github.dgautier.navigator.ICNUtils;
+import com.github.dgautier.navigator.model.JsonResponse;
+import com.google.common.base.Preconditions;
+import org.apache.http.Consts;
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.CookieStore;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.protocol.ClientContext;
+import org.apache.http.impl.client.BasicCookieStore;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.protocol.BasicHttpContext;
+import org.apache.http.protocol.HttpContext;
+import org.apache.http.util.EntityUtils;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+@Mojo(name = "deployPlugin", defaultPhase = LifecyclePhase.PRE_INTEGRATION_TEST)
+public class DeployPlugin extends CopyTargetMojo {
+
+    /**
+     * An ICN user with Admin rights
+     */
+    @Parameter(defaultValue = "p8admin", property = "adminICNUser", required = true)
+    private String adminICNUser;
+
+    /**
+     * An ICN user with Admin rights password's
+     */
+    @Parameter(defaultValue = "p8admins", property = "adminICNPassword", required = true)
+    private String adminICNPassword;
+
+    /**
+     * ICN Context Root path
+     */
+    @Parameter(defaultValue = "http://localhost/navigator", property = "navigatorUrl", required = true)
+    private String navigatorUrl;
+
+    @Parameter(property = "configuration", required = false)
+    private String configuration;
+
+
+    private static final HttpClient httpClient = new DefaultHttpClient();
+    private static final HttpContext localContext = new BasicHttpContext();
+    private static final CookieStore cookieStore = new BasicCookieStore();
+    private String securityToken = null;
+
+
+    public DeployPlugin() {
+        localContext.setAttribute(ClientContext.COOKIE_STORE, cookieStore);
+    }
+
+    @Override
+    public void execute() throws MojoExecutionException {
+        super.execute();
+
+
+        try {
+            copyFileToDest();
+            logonICN();
+            uploadPlugin();
+            savePlugin();
+        } catch (IOException e) {
+            throw new MojoExecutionException("Failed to copy file to Dest", e);
+        } catch (Exception e) {
+            throw new MojoExecutionException(e.getMessage(), e);
+        }
+
+    }
+
+    @Override
+    protected void onCheckPreconditions() {
+        super.onCheckPreconditions();
+        Preconditions.checkNotNull(adminICNUser, "An ICN user with Admin rights must be set in plugin's configuration.");
+        Preconditions.checkNotNull(adminICNPassword, "An ICN user password's must be set in plugin's configuration.");
+        Preconditions.checkNotNull(navigatorUrl, "ICN Url must be set in plugin's configuration.");
+        Preconditions.checkArgument(!navigatorUrl.endsWith("/"), "ICN Url must not be ended with /");
+
+    }
+
+    @Override
+    protected void initConnection() {
+        super.initConnection();
+    }
+
+    @Override
+    protected void copyFileToDest() throws IOException {
+        super.copyFileToDest();
+    }
+
+    protected JsonResponse logonICN() throws Exception {
+        try {
+            getLog().debug("Logging to ICN : " + navigatorUrl);
+            HttpPost post = new HttpPost(navigatorUrl + "/container/logon.do");
+//			HttpPost post = new HttpPost(navigatorUrl+"/p8/logon.do");
+            ICNUtils.addFormHeader(post);
+
+            List<NameValuePair> nvps = new ArrayList<NameValuePair>();
+
+            // FIXME desktop could be extdernalized
+            nvps.add(new BasicNameValuePair("desktop", "admin"));
+            nvps.add(new BasicNameValuePair("userid", adminICNUser));
+            nvps.add(new BasicNameValuePair("password", adminICNPassword));
+
+            post.setEntity(new UrlEncodedFormEntity(nvps, Consts.UTF_8));
+
+            HttpResponse httpResponse = httpClient.execute(post, localContext);
+
+            JsonResponse response = getResponse(httpResponse);
+            Preconditions.checkNotNull(response.getSecurity_token());
+            this.securityToken = response.getSecurity_token();
+            getLog().debug("Security Token retrieved : " + this.securityToken);
+            return response;
+        } catch (Exception e) {
+            getLog().error(e.getMessage(), e);
+            throw e;
+        }
+    }
+
+    private void checkResponseCodeOK(HttpResponse httpResponse) {
+        Preconditions.checkArgument(HttpStatus.SC_OK == httpResponse.getStatusLine().getStatusCode(), "Invalid response code " + httpResponse.getStatusLine().getStatusCode());
+    }
+
+    private JsonResponse getResponse(HttpResponse httpResponse) throws IOException {
+        checkResponseCodeOK(httpResponse);
+        String responseEntity = EntityUtils.toString(httpResponse.getEntity());
+        String responseJSON = ICNUtils.cleanResponse(responseEntity);
+        JsonResponse response = ICNUtils.fromJSON(responseJSON);
+        Preconditions.checkNotNull(response);
+
+        // Ensure we have no errors
+        Preconditions.checkArgument(!response.hasErrors(), response.getErrorMessage());
+
+        // Display message if exists
+        if (response.hasMessages()) {
+            getLog().info(response.getInfoMessage());
+        }
+        return response;
+    }
+
+
+    protected JsonResponse savePlugin() throws Exception {
+        getLog().info("Saving plugin configuration : ");
+        try {
+            getLog().info(getJarToDeploy().getAbsolutePath());
+
+            AbstractPlugin plugin = ICNUtils.loadFromJar(getJarToDeploy());
+            plugin.setFilename(getRemoteFile().getPath());
+
+
+            if (this.configuration != null) {
+                getLog().info("Found configuration : " + configuration);
+                plugin.setConfiguration(this.configuration);
+            } else {
+                getLog().info("No plugin configuration defined.");
+            }
+
+            HttpPost httpPost = new HttpPost(navigatorUrl + "/admin/configuration.do");
+            ICNUtils.addFormHeader(httpPost);
+
+            List<NameValuePair> nvps = new ArrayList<NameValuePair>();
+
+
+            // Save plugin configuration
+            nvps.add(new BasicNameValuePair("action", "add"));
+            nvps.add(new BasicNameValuePair("id", plugin.getId()));
+            nvps.add(new BasicNameValuePair("configuration", "PluginConfig"));
+            nvps.add(new BasicNameValuePair("application", "navigator"));
+            nvps.add(new BasicNameValuePair("update_list_configuration", "ApplicationConfig"));
+            nvps.add(new BasicNameValuePair("update_list_id", "navigator"));
+            nvps.add(new BasicNameValuePair("update_list_type", "plugins"));
+            nvps.add(new BasicNameValuePair("security_token", this.securityToken));
+
+            String pluginJSON = ICNUtils.getPluginJSON(plugin);
+            nvps.add(new BasicNameValuePair("json_post", pluginJSON));
+            nvps.add(new BasicNameValuePair("desktop", "admin"));
+
+
+            httpPost.setEntity(new UrlEncodedFormEntity(nvps, Consts.UTF_8));
+
+            HttpResponse httpResponse = httpClient.execute(httpPost, localContext);
+            JsonResponse response = getResponse(httpResponse);
+            return response;
+
+        } catch (Exception e) {
+            getLog().error(e.getMessage(), e);
+            throw e;
+        }
+    }
+
+    /**
+     * Ensure that there is no validation issue
+     *
+     * @return
+     * @throws java.io.IOException
+     */
+    protected JsonResponse uploadPlugin() throws Exception {
+        getLog().debug("Loading plugin");
+        try {
+            HttpPost httpPost = new HttpPost(navigatorUrl + "/admin/loadPlugin.do");
+            ICNUtils.addFormHeader(httpPost);
+
+            List<NameValuePair> nvps = new ArrayList<NameValuePair>();
+            nvps.add(new BasicNameValuePair("desktop", "admin"));
+            nvps.add(new BasicNameValuePair("userid", adminICNUser));
+            nvps.add(new BasicNameValuePair("security_token", this.securityToken));
+            getLog().debug("Remote File Path : " + getRemoteFile().getPath());
+            nvps.add(new BasicNameValuePair("fileName", getRemoteFile().getPath()));
+
+            httpPost.setEntity(new UrlEncodedFormEntity(nvps, Consts.UTF_8));
+
+            HttpResponse httpResponse = httpClient.execute(httpPost, localContext);
+            JsonResponse response = getResponse(httpResponse);
+            return response;
+
+        } catch (Exception e) {
+            getLog().error(e.getMessage(), e);
+            throw e;
+        }
+    }
+
+    public void setAdminICNUser(String adminICNUser) {
+        this.adminICNUser = adminICNUser;
+    }
+
+    public void setAdminICNPassword(String adminICNPassword) {
+        this.adminICNPassword = adminICNPassword;
+    }
+
+    public void setNavigatorURL(String navigatorUrl) {
+        this.navigatorUrl = navigatorUrl;
+    }
+
+    public void setConfiguration(String configuration) {
+        this.configuration = configuration;
+    }
+}
